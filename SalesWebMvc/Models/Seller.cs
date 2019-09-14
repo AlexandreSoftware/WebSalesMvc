3c6a4d25e43cc28ff29548141d6a527df2a19c3f
Carlos Oliveira <Oliveira_carlos2017@outlook.com.br>
2019-09-14 11:21:18 -0300

    First commit- comecando testes com controllers

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models {
+    public class Seller {
+        public Department department { get; private set; }
+        internal void removedepartment() {
+            department = null;
+        }
+    }
+}
